c6ecbb63f67567c3d5e58d8350376af27ce3a761
Youssef Dybala <newyossef55o@gmail.com>
2024-09-18 01:57:22 +0300

    new commit

--- js/script.js
+++ js/script.js
@@ -41,3 +41,21 @@ var swiper = new Swiper(".mySwiper", {
     },
   });
   //////////////////// end test 
+
+  /////////////////////// start pricing 
+let yearly = document.querySelector(".yearly")
+let monthly = document.querySelector(".monthly")
+let priceOne = document.querySelector(".price-One")
+let priceTwo = document.querySelector(".price-Two")
+let priceThree = document.querySelector(".price-Three")
+monthly.onclick = function () {
+  priceOne.textContent = "$29.50"
+  priceTwo.textContent = "$44.40"
+  priceThree.textContent = "$70.50"
+  yearly.onclick = function () {
+    priceOne.textContent = "$350.70"
+    priceTwo.textContent = "$530.60"
+    priceThree.textContent = "$840.30"
+  }
+}
+  /////////////////////// end pricing 
