daf1c9b696f5658f658e991059b163cdd6599609
Youssef Dybala <newyossef55o@gmail.com>
2024-09-19 05:13:03 +0300

    new commit

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 /////////////// start nav
 let dropDownNav = document.querySelector(".open-nav")
-let menuNav = document.querySelector(".drop-down")
+let megaMenu = document.querySelector(".mega-menu")
 let closeNav = document.querySelector(".close-nav")
 dropDownNav.addEventListener("click" , function () {
-  menuNav.classList.add("open-navv")
+    megaMenu.classList.add("open-navv")
   })
   closeNav.addEventListener("click" , function () {
-    menuNav.classList.remove("open-navv")
+    megaMenu.classList.remove("open-navv")
   })
 /////////////// end nav
 
